66508eb087cc9543c2e0ef9264da90278ac39ab0
DanielMachado65 <danielmachadopintos@gmail.com>
2018-09-06 09:59:38 -0300

    tentativa de autenticaçã
    Para ser possivel atenticar o aplicativo

--- app/main/routes.py
+++ app/main/routes.py
@@ -2,8 +2,17 @@ from flask import render_template, Blueprint
 
 main = Blueprint('main', __name__)
 
+lista=[
+    {
+        'item1': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur esse illum iusto natus vitae? Aspernatur dolores expedita iure iusto nam recusandae voluptas? Aspernatur necessitatibus nobis perferendis sit veritatis voluptatum? Doloremque?'
+    },
+    {
+        'item1': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur esse illum iusto natus vitae? Aspernatur dolores expedita iure iusto nam recusandae voluptas? Aspernatur necessitatibus nobis perferendis sit veritatis voluptatum? Doloremque?'
+    }
+]
+
 
 @main.route('/')
 @main.route('/home')
 def home():
-    return render_template('home.html', title='Home'), 200
+    return render_template('home.html', title='Home', list=lista), 200
--- app/processo/routes.py
+++ app/processo/routes.py
@@ -1,5 +1,7 @@
 import dialogflow_v2 as dialogflow
+
 from flask import Blueprint
+from google.cloud import storage
 
 processo = Blueprint('processo', __name__)
 
@@ -7,26 +9,22 @@ processo = Blueprint('processo', __name__)
 @processo.route('/processo')
 def processoMain():
     try:
-        detect_intent_texts('testebank-9465d', '231', 'abrir um processo','pt-br')
+        implicit()
+        print('oi')
+        # TODO: detect_intent_texts('testebank-9465d', 'rando-string', 'abrir um processo', 'pt-br')
     except Exception as erro:
         print(erro)
     return 'ok'
 
 
 def detect_intent_texts(project_id, session_id, texts, language_code):
-    """Returns the result of detect intent with texts as inputs.
-
-    Using the same `session_id` between requests allows continuation
-    of the conversaion."""
-    import dialogflow_v2 as dialogflow
     session_client = dialogflow.SessionsClient()
 
     session = session_client.session_path(project_id, session_id)
     print('Session path: {}\n'.format(session))
 
     for text in texts:
-        text_input = dialogflow.types.TextInput(
-            text=text, language_code=language_code)
+        text_input = dialogflow.types.TextInput(text=text, language_code=language_code)
 
         query_input = dialogflow.types.QueryInput(text=text_input)
 
@@ -40,3 +38,14 @@ def detect_intent_texts(project_id, session_id, texts, language_code):
             response.query_result.intent_detection_confidence))
         print('Fulfillment text: {}\n'.format(
             response.query_result.fulfillment_text))
+
+
+def implicit():
+    # If you don't specify credentials when constructing the client, the
+    # client library will look for credentials in the environment.
+    storage_client = storage.Client()
+    print(storage_client)
+
+    # Make an authenticated API request
+    buckets = list(storage_client.list_buckets())
+    print(buckets)
