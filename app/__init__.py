9b2456cc36376f29578cbf8208bdd14145857f1f
DanielMachado65 <danielmachadopintos@gmail.com>
2018-09-06 14:01:11 -0300

    inserção da manipulação com API
    Inserindo os códigos com a API

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from google.cloud import storage
 
 from app.config import Config
 
@@ -14,5 +15,15 @@ def create_app(config_class=Config):
     app.register_blueprint(main)
     app.register_blueprint(errors)
     app.register_blueprint(processo)
-
+    implicit()
     return app
+
+
+def implicit():
+    # If you don't specify credentials when constructing the client, the
+    # client library will look for credentials in the environment.
+    storage_client = storage.Client()
+
+    # Make an authenticated API request
+    buckets = list(storage_client.list_buckets())
+    print(buckets)
--- app/processo/routes.py
+++ app/processo/routes.py
@@ -1,20 +1,16 @@
 import dialogflow_v2 as dialogflow
 
-from flask import Blueprint
-from google.cloud import storage
+from flask import Blueprint, render_template
 
 processo = Blueprint('processo', __name__)
 
 
 @processo.route('/processo')
 def processoMain():
-    try:
-        implicit()
-        print('oi')
-        # TODO: detect_intent_texts('testebank-9465d', 'rando-string', 'abrir um processo', 'pt-br')
-    except Exception as erro:
-        print(erro)
-    return 'ok'
+    return render_template('home.html',
+                        texto=detect_intent_texts(project_id='small-talk-c36ba', session_id=12, texts='abrir um proceso', language_code='pt-br'))
+
+
 
 
 def detect_intent_texts(project_id, session_id, texts, language_code):
@@ -22,30 +18,15 @@ def detect_intent_texts(project_id, session_id, texts, language_code):
 
     session = session_client.session_path(project_id, session_id)
     print('Session path: {}\n'.format(session))
+    text_input = dialogflow.types.TextInput(text=texts, language_code=language_code)
 
-    for text in texts:
-        text_input = dialogflow.types.TextInput(text=text, language_code=language_code)
+    query_input = dialogflow.types.QueryInput(text=text_input)
 
-        query_input = dialogflow.types.QueryInput(text=text_input)
+    response = session_client.detect_intent(session=session, query_input=query_input)
 
-        response = session_client.detect_intent(
-            session=session, query_input=query_input)
-
-        print('=' * 20)
-        print('Query text: {}'.format(response.query_result.query_text))
-        print('Detected intent: {} (confidence: {})\n'.format(
-            response.query_result.intent.display_name,
-            response.query_result.intent_detection_confidence))
-        print('Fulfillment text: {}\n'.format(
-            response.query_result.fulfillment_text))
-
-
-def implicit():
-    # If you don't specify credentials when constructing the client, the
-    # client library will look for credentials in the environment.
-    storage_client = storage.Client()
-    print(storage_client)
-
-    # Make an authenticated API request
-    buckets = list(storage_client.list_buckets())
-    print(buckets)
+    print('-_-' * 20)
+    print('Query text: {}'.format(response.query_result.query_text))
+    return 'Frase Verificada: {} <hr><br> Detected intent: {} (confidence: {})\n'.format(
+        response.query_result.fulfillment_text,
+        response.query_result.intent.display_name,
+        response.query_result.intent_detection_confidence)
